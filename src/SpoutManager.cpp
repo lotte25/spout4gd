ed58f7edc6cf3adc8e3933307301d85fff8e783a
Lotte25 <loldebi5@gmail.com>
2026-02-11 17:32:40 -0300

    chore?: remove unused (for now) player capture code

--- src/SpoutManager.cpp
+++ src/SpoutManager.cpp
@@ -2,15 +2,7 @@
 #include "SpoutTarget.hpp"
 
 namespace SpoutManager {
-    static SpoutTarget mainTarget("Geometry Dash - Screen");
-    // SpoutTarget playerTarget("Geometry Dash - Player");
-
-/*  SimplePlayer* playerClone = nullptr;
-    int lastIconID = -1;
-    IconType lastIconType = IconType::Cube;
-    int lastCol1 = -1;
-    int lastCol2 = -1;
-    const int PLAYER_RES = 512; */
+    SpoutTarget mainTarget("Geometry Dash - Screen");
 
     using clock = std::chrono::steady_clock;
     using duration = std::chrono::duration<double>;
@@ -59,118 +51,6 @@ namespace SpoutManager {
 
         mainTarget.send();
     }
-
-/*     void capturePlayer() {
-        auto pl = PlayLayer::get();
-        if (!pl || !pl->m_player1) return;
-        auto player = pl->m_player1;
-        auto gm = GameManager::sharedState();
-
-        playerTarget.ensureSize(PLAYER_RES, PLAYER_RES);
-
-        IconType currentType = IconType::Cube;
-        int iconID = gm->getPlayerFrame();
-
-        if (player->m_isShip) {
-            currentType = IconType::Ship;
-            iconID = gm->getPlayerShip();
-        }
-        else if (player->m_isBall) {
-            currentType = IconType::Ball;
-            iconID = gm->getPlayerBall();
-        }
-        else if (player->m_isBird) {
-            currentType = IconType::Ufo;
-            iconID = gm->getPlayerBird();
-        }
-        else if (player->m_isDart) {
-            currentType = IconType::Wave;
-            iconID = gm->getPlayerDart();
-        }
-        else if (player->m_isRobot) {
-            currentType = IconType::Robot;
-            iconID = gm->getPlayerRobot();
-        }
-        else if (player->m_isSpider) {
-            currentType = IconType::Spider;
-            iconID = gm->getPlayerSpider();
-        }
-        else if (player->m_isSwing) {
-            currentType = IconType::Swing;
-            iconID = gm->getPlayerSwing();
-        }
-
-        int col1 = gm->getPlayerColor();
-        int col2 = gm->getPlayerColor2();
-
-        if (!playerClone) {
-            playerClone = SimplePlayer::create(iconID);
-            playerClone->retain();
-            playerClone->setAnchorPoint({ 0.5f, 0.5f });
-        }
-
-        if (iconID != lastIconID || currentType != lastIconType) {
-            playerClone->updatePlayerFrame(iconID, currentType);
-            lastIconID = iconID;
-            lastIconType = currentType;
-        }
-
-        if (col1 != lastCol1 || col2 != lastCol2) {
-            playerClone->setColor(gm->colorForIdx(col1));
-            playerClone->setSecondColor(gm->colorForIdx(col2));
-            lastCol1 = col1;
-            lastCol2 = col2;
-        }
-
-        playerClone->updateColors();
-
-        playerClone->setRotation(player->getRotation());
-        playerClone->setOpacity(255);
-        playerClone->setScale(3.f);
-
-        playerClone->setPosition(ccp(PLAYER_RES / 2.0f, PLAYER_RES / 2.0f));
-
-        if (gm->getPlayerGlow()) {
-            playerClone->setGlowOutline(gm->colorForIdx(col1)); // O el color de glow que corresponda
-        } else {
-            playerClone->disableGlowOutline();
-        }
-
-        GLint oldFBO;
-        glGetIntegerv(GL_FRAMEBUFFER_BINDING, &oldFBO);
-        GLint oldViewport[4];
-        glGetIntegerv(GL_VIEWPORT, oldViewport);
-
-        glBindFramebuffer(GL_FRAMEBUFFER, playerTarget.fbo);
-        glViewport(0, 0, PLAYER_RES, PLAYER_RES);
-        glClearColor(0, 0, 0, 0);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        kmGLMatrixMode(KM_GL_PROJECTION);
-        kmGLPushMatrix();
-        kmGLLoadIdentity();
-
-        kmMat4 orthoMatrix;
-        kmMat4OrthographicProjection(&orthoMatrix, 0, PLAYER_RES, 0, PLAYER_RES, -1024, 1024);
-        kmGLMultMatrix(&orthoMatrix);
-
-        kmGLMatrixMode(KM_GL_MODELVIEW);
-        kmGLPushMatrix();
-        kmGLLoadIdentity();
-
-        playerClone->visit();
-
-        kmGLMatrixMode(KM_GL_MODELVIEW);
-        kmGLPopMatrix();
-
-        kmGLMatrixMode(KM_GL_PROJECTION);
-        kmGLPopMatrix();
-
-        glBindFramebuffer(GL_FRAMEBUFFER, oldFBO);
-        glViewport(oldViewport[0], oldViewport[1], oldViewport[2], oldViewport[3]);
-
-        playerTarget.send();
-    } */
 
     void reset() {
         mainTarget.cleanup();
